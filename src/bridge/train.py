d49c938a2a0ebe940f403f5d16a7e92f80d4c003
Michał Wójcik <michwoj01@gmail.com>
2025-06-15 19:49:18 +0200

    improve training

--- src/bridge/train.py
+++ src/bridge/train.py
@@ -14,8 +14,6 @@ from rlcard.utils import (
 )
 from rlcard.envs.registration import register
 
-from shared_dqn_agent import SharedDQNAgent
-
 
 def train(args):
     set_seed(args.seed)
@@ -28,17 +26,17 @@ def train(args):
     env = rlcard.make('crew', config={'seed': args.seed})
     device = get_device()
 
-    # Create shared agent
-    shared_agent = DQNAgent(
-                    num_actions=env.num_actions,
-                    state_shape=env.state_shape[0],
-                    mlp_layers=[64, 64],
-                    device=device,
-                    save_path=args.log_dir,
-                    save_every=args.save_every
-                )
-
-    agents = [shared_agent for _ in range(env.num_players)]
+    agents = [
+        DQNAgent(
+            num_actions=env.num_actions,
+            state_shape=env.state_shape[0],
+            mlp_layers=[64, 64],
+            device=device,
+            save_path=args.log_dir,
+            save_every=args.save_every
+        )
+        for _ in range(env.num_players)
+    ]
     env.set_agents(agents)
 
     # Start training
@@ -46,73 +44,35 @@ def train(args):
         for episode in range(1, args.num_episodes + 1):
             print(f'Episode: {episode}')
             trajectories, payoffs = env.run(is_training=True)
-
-            # Reorganize the data to be state, action, reward, next_state, done
             trajectories = reorganize(trajectories, payoffs)
 
-            # Feed transitions into agent memory, and train the agent
-            # Here, we assume that DQN always plays the first position
-            # and the other players play randomly (if any)
             for player_id, trajectory in enumerate(trajectories):
                 for ts in trajectory:
-                    shared_agent.feed(ts)
+                    agents[player_id].feed(ts)
 
-            # Evaluate the performance. Play with random agents.
             if episode % args.evaluate_every == 0:
                 performance = tournament(env, args.num_eval_games)
                 logger.log_performance(episode, np.mean(performance))
 
-    # Save model
-    model_path = os.path.join(args.log_dir, 'shared_dqn_model.pth')
-    shared_agent.save(model_path)
-    print('Model saved at:', model_path)
+    for i, agent in enumerate(agents):
+        model_path = os.path.join(args.log_dir, f'dqn_model_player_{i}.pth')
+        agent.save(model_path)
+        print(f'Model gracza {i} zapisany w: {model_path}')
 
     plot_curve(logger.csv_path, logger.fig_path, args.algorithm)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '--cuda',
-        type=str,
-        default='',
-    )
-    parser.add_argument(
-        '--seed',
-        type=int,
-        default=42,
-    )
-    parser.add_argument(
-        '--num_episodes',
-        type=int,
-        default=3000,
-    )
-    parser.add_argument(
-        '--num_eval_games',
-        type=int,
-        default=1000,
-    )
-    parser.add_argument(
-        '--evaluate_every',
-        type=int,
-        default=100,
-    )
-    parser.add_argument(
-        '--log_dir',
-        type=str,
-        default='experiments/',
-    )
-
-    parser.add_argument(
-        "--load_checkpoint_path",
-        type=str,
-        default="",
-    )
-
-    parser.add_argument(
-        "--save_every",
-        type=int,
-        default=-1)
+    parser.add_argument('--cuda', type=str, default='')
+    parser.add_argument('--seed', type=int, default=42)
+    parser.add_argument('--num_episodes', type=int, default=3000)
+    parser.add_argument('--num_eval_games', type=int, default=1000)
+    parser.add_argument('--evaluate_every', type=int, default=100)
+    parser.add_argument('--log_dir', type=str, default='experiments/')
+    parser.add_argument("--load_checkpoint_path", type=str, default="")
+    parser.add_argument("--save_every", type=int, default=-1)
+    parser.add_argument("--algorithm", type=str, default="dqn")
 
     args = parser.parse_args()
 
