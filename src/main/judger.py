5d5c9ef591c2ee368e12e1103e459f5769a1f7e4
strjakub <strojewski.jakub@gmail.com>
2025-08-06 21:30:26 +0200

    fix judger

--- src/main/judger.py
+++ src/main/judger.py
@@ -53,8 +53,7 @@ class Judger:
                 if trick_moves and len(trick_moves) < 4:
                     led_card: CrewCard = trick_moves[0].card
                     cards_of_led_suit = [card for card in hand
-                                         if card.suit == led_card.suit
-                                         or card.suit == CrewCard.trump_suit]
+                                         if card.suit == led_card.suit]
                     if cards_of_led_suit:
                         legal_cards = cards_of_led_suit
                 for card in legal_cards:
