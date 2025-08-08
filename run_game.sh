f09f2570e809bc5cddff925284b1cfaee13de88a
Michał Wójcik <michwoj01@gmail.com>
2025-08-08 18:30:47 +0200

    fix run script

--- run_game.sh
+++ run_game.sh
@@ -2,11 +2,11 @@
 
 # add one arg to script
 if [ $# -ne 1 ]; then
-    echo "Usage: $0 <number_of_tasks>"
+    echo "Usage: $0 <number_of_tasks> from 1 to 4"
     exit 1
 fi
 NO_TASKS=$1
 
 export PYTHONPATH=$(dirname "$0")
 
-python3 src/rlcard/human_train.py $NO_TASKS
+python3 human_train.py $NO_TASKS
